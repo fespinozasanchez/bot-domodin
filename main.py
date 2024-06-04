e411dca0a7262177f984a88a0341f5defbb8b727
Felipe Espinoza <53890265+fespinozasanchez@users.noreply.github.com>
2024-06-04 00:38:08 -0400

    Update main.py
    add: new function

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from discord.ext import commands
 import os
 import logging
 import random as ra
+from discord import FFmpegPCMAudio
+import yt_dlp as youtube_dl
+import glob
+from mutagen.mp3 import MP3
 
 
 handler = logging.FileHandler(
@@ -67,14 +71,14 @@ async def info(ctx):
     embed.add_field(name="Áreas de Docencia",
                     value="·Programación II.\n·Programación Integración de Sistemas.\n·Arquitectura de Hardware.\n·Micro Controladores.\n·Interfaces Gráficas.", inline=True)
     embed.add_field(name="Otros Antecedentes", value="·Director del Centro de Desarrollo de Software y Tecnología – CEDEST.\n·Docente del plan especial de ingeniería informática para técnicos.\n·Profesor programa de Magister de Matemáticas de Universidad Católica de Temuco.\n·Expositor permanente de Casa Abierta y vínculo con colegios y liceos de la zona.\n·Coordinador responsable evento OCI-2018", inline=False)
-    embed.add_field(name="Publicaciones", value=""" 
+    embed.add_field(name="Publicaciones", value="""
         ·Billy Peralta, Luis Alberto Caro , Alvaro Soto – Unsupervised Local Regressive Attributes for Pedestrian Re-Identification.
         ·Billy Peralta, Ariel Saavedra, Luis Alberto Caro – A proposal for mixture of experts with entropic regularization.
         ·Billy Peralta, Luis Alberto Caro – Improved Object Recognition with Decision Trees Using Subspace Clustering.
         ·Billy Peralta, Luis Alberto Caro, Alvaro Soto – A proposal for supervised clustering with Dirichlet Process using labels.
         ·Billy Peralta, L. Parra, Luis Alberto Caro – Evaluation of stacked auto-encoders for pedestrian detection.
         """, inline=True)
-    embed.add_field(name="Publicaciones", value="""        
+    embed.add_field(name="Publicaciones", value="""
         ·Billy Peralta, T. Poblete, Luis Alberto Caro – Automatic feature selection for desertion and graduation prediction: A Chilean case.
         ·Luis Alberto Caro, Camilo Silva, Billy Peralta, Oriel A. Herrera, Sergio Barrientos – Real-Time Recognition of Arm motion Using Artificial Neural Network Multi-perceptron with Arduino One MicroController and EKG/EMG Shield Sensor.
         ·Luis Alberto Caro, Javier Correa, Pablo Espinace, Daniel Langdon, Daniel Maturana, Rubén Mitnik, Sebastian Montabone, Stefan Pszczólkowski, Anita Araneda, Domingo Mery, Miguel Torres, Alvaro Soto – Indoor Mobile Robotic at Grima, PUC.
@@ -85,6 +89,149 @@ async def info(ctx):
     embed.set_thumbnail(url="https://www.inf.uct.cl/wp-content/uploads/2018/10/Luis-Caro-577x1024.jpg")
     await ctx.send(embed=embed)
 
+# Deja de webear
+@bot.command()
+async def interfaz(ctx):
+    embed = discord.Embed()
+    embed.title = "NO ME INTERESA LA INTERFACE"
+    embed.description = "DEJAAA DE WEBEA CON LA INTERFAZ REQL, YO EVALUO LOS ALGORITMOS!!!!"
+    embed.color = discord.Color.from_rgb(255,0,0)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def pasa(ctx, user: discord.User):
+    resultado = ra.randint(0, 1)
+    if resultado == 1:
+        await ctx.send(f"{user.mention} ¡Estás aprobado! 🎉")
+    else:
+        await ctx.send(f"{user.mention} Lamentablemente!, repruebas el ramo malo ql sin opción a repetir. 😢")
+
+
+@bot.command()
+async def aram(ctx):
+    result = ra.randint(0,1)
+    if result == 1:
+        await ctx.send('@everyone ¡Hora de jugar ARAM! 🎮')
+    else:
+        await ctx.send('Hoy no se juega!')
+
+
+@bot.command()
+async def quesehace(ctx):
+    await ctx.send('@everyone\nHoy solo se descansa gracias.')
+
+
+# conectar a un canal de voz
+
+
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice is None:
+        await ctx.send("No estas conectado a ningun canal de voz")
+        return
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+# desconectar del canal de voz
+
+
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+
+@bot.command()
+async def play(ctx, filename: str, repeat: int = 1):
+    if ctx.voice_client is None:
+        await ctx.send("No estoy conectado a un canal de voz")
+        return
+
+    # Validar y asegurar que el número máximo de repeticiones sea 10
+    try:
+        repeat = abs(int(repeat))
+        if repeat < 1:
+            raise ValueError
+        repeat = min(repeat, 10)
+    except (ValueError, SyntaxError, NameError):
+        await ctx.send("ESCRIBE EL NUMERO BIEN SACO WEA")
+        return
+
+    # Definir el directorio donde se encuentran los archivos de audio
+    directory = '/usr/home/users/fespinoza/bot_c/audio'
+    # Buscar archivos que coincidan con el nombre del archivo proporcionado (sin extensión)
+    file_path = glob.glob(os.path.join(directory, f"{filename}.*"))
+
+    if not file_path:
+        await ctx.send("El archivo especificado no existe.")
+        return
+
+    vc = ctx.voice_client
+    bot.is_playing_audio = True
+
+    # Función para reproducir el audio repetidamente
+    def play_next(repeat_count):
+        if repeat_count > 0 and bot.is_playing_audio:
+            vc.play(FFmpegPCMAudio(file_path[0]), after=lambda e: play_next(repeat_count - 1))
+
+    if not vc.is_playing():
+        play_next(repeat)
+    else:
+        await ctx.send("Ya estoy reproduciendo audio.")
+
+
+@bot.command()
+async def stop(ctx):
+    if ctx.voice_client is None:
+        await ctx.send("No estoy conectado a un canal de voz")
+        return
+
+    if ctx.voice_client.is_playing():
+        bot.is_playing_audio = False
+        ctx.voice_client.stop()
+        await ctx.send("Reproducción detenida.")
+    else:
+        await ctx.send("No estoy reproduciendo ningún audio.")
+
+
+@bot.command()
+async def list(ctx):
+    directory = '/usr/home/users/fespinoza/bot_c/audio'
+    embed = discord.Embed(
+        title="Lista de reproducción disponible",
+        url="https://media.licdn.com/dms/image/C5603AQHFwyRGQtuSUA/profile-displayphoto-shrink_200_200/0/1516997163523?e=2147483647&v=beta&t=WtAtj17uSKfW4cIb1Ki8o4fBeqXTOnR4qooq9wSb8zI",
+        description="Uso: !play fileName",
+        color=0xFF5733
+    )
+    embed.set_author(
+        name="Dr(c) Luis Alberto Caro",
+        url="https://media.licdn.com/dms/image/C5603AQHFwyRGQtuSUA/profile-displayphoto-shrink_200_200/0/1516997163523?e=2147483647&v=beta&t=WtAtj17uSKfW4cIb1Ki8o4fBeqXTOnR4qooq9wSb8zI"
+    )
+
+    if not os.path.exists(directory):
+        await ctx.send("El directorio especificado no existe.")
+        return
+
+    files = os.listdir(directory)
+
+    if not files:
+        await ctx.send("El directorio está vacío.")
+        return
+
+    i = 1
+    for file in files:
+        file_path = os.path.join(directory, file)
+        if file.endswith('.mp3'):
+            audio = MP3(file_path)
+            duration = int(audio.info.length)
+            minutes, seconds = divmod(duration, 60)
+            duration_str = f"{minutes}:{seconds:02d}"
+            file_name = file.split('.')[0]
+            embed.add_field(name="", value=f'{i}. {file_name} - ({duration_str})', inline=True)
+            i += 1
+
+    embed.set_footer(text="Lunes – Viernes: 6:00 – ??:??")
+    embed.set_thumbnail(url="https://media.licdn.com/dms/image/C5603AQHFwyRGQtuSUA/profile-displayphoto-shrink_200_200/0/1516997163523?e=2147483647&v=beta&t=WtAtj17uSKfW4cIb1Ki8o4fBeqXTOnR4qooq9wSb8zI")
+    await ctx.send(embed=embed)
 
 # Hello
 @bot.command()
@@ -99,6 +246,10 @@ async def hello(ctx):
     # await ctx.send('Hola mis estudiantes, vengan a estudiar conmigo, Pascal un potententisimo lenguaje de programación')
 # https://www.youtube.com/watch?v=yvFCI2whgOA
 
+@bot.command()
+async def diuca(ctx):
+    await ctx.send("ESTOY CANSADO JEFE\nSaquenme de acªªªªªªªªªªªªªªªªªªªªªªªªªªªªªªªªªªªªªªªªª")
+    await ctx.send("https://pillan.inf.uct.cl/~fespinoza/file.jpg")
 
 @bot.event
 async def on_ready():
