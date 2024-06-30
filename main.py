eb89ea7dc3e0d7c8f4a928acb4c079a50e643163
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2024-06-30 01:21:08 -0400

    chore: Enable member intents in Discord bot
    ```

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ token = os.getenv('DISCORD_TOKEN')
 
 # Configurar intentos de Discord
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 
 # Crear instancia del bot
@@ -39,15 +40,11 @@ reminder_manager = ReminderManager()
 fun_commands.register_commands(bot)
 music_commands.register_commands(bot, audio_queue)
 reminder_commands.register_commands(bot, reminder_manager)
-# chat_commands.register_commands(bot)
-
-# Función asíncrona para cargar los cogs
 
 
 async def load_cogs():
     await bot.load_extension('cogs.hangman')
-    await bot.load_extension('features.betting_system')  # Cargar el nuevo cog
-    # Cargar el nuevo cog de economía
+    await bot.load_extension('features.betting_system')
     await bot.load_extension('features.economy')
 
 
@@ -64,11 +61,8 @@ async def on_ready():
 
 
 async def main():
-    # Configurar el logger
     setup_logger()
-    # Cargar los cogs
     await load_cogs()
-    # Ejecutar el bot
     await bot.start(token)
 
 if __name__ == '__main__':
