8900d66f71ba66330e74aa0b897837664f214cc7
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2024-06-30 01:22:10 -0400

    chore: Refactor ReminderManager to use database for managing reminders

--- utils/reminder_manager.py
+++ utils/reminder_manager.py
@@ -1,18 +1,18 @@
-from datetime import datetime, timedelta
+from datetime import datetime
+from utils.data_manager import save_reminder, load_reminders, delete_reminder
 
 
 class ReminderManager:
     def __init__(self):
-        self.reminders = []
+        self.reminders = load_reminders()
 
     def add_reminder(self, reminder_datetime, message, channel_id):
-        self.reminders.append((reminder_datetime, message, channel_id))
+        save_reminder(reminder_datetime, message, channel_id)
+        self.reminders = load_reminders()
 
-    def remove_reminder(self, index):
-        if 0 <= index < len(self.reminders):
-            return self.reminders.pop(index)
-        else:
-            return None
+    def remove_reminder(self, reminder_id):
+        delete_reminder(reminder_id)
+        self.reminders = load_reminders()
 
     def get_reminders(self):
         return self.reminders
@@ -20,9 +20,11 @@ class ReminderManager:
     async def check_reminders(self, bot):
         now = datetime.now()
         for reminder in self.reminders[:]:
-            reminder_time, message, channel_id = reminder
+            reminder_time = reminder['reminder_time']
+            message = reminder['message']
+            channel_id = reminder['channel_id']
             if now >= reminder_time:
                 channel = bot.get_channel(channel_id)
-                for i in range(10):
-                    await channel.send(f"@everyone son las {reminder_time.strftime('%H:%M')} hora, es hora {message}")
-                self.reminders.remove(reminder)
+                await channel.send(f"@everyone son las {reminder_time.strftime('%H:%M')} hora, es hora {message}")
+                delete_reminder(reminder['id'])
+                self.reminders = load_reminders()
