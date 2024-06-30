0d7863fb81ee7d67f762622a438fee3422fd4686
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2024-06-30 01:21:51 -0400

    chore: Update economy.py to register users on bot ready event and fix key formation

--- features/economy.py
+++ features/economy.py
@@ -1,6 +1,10 @@
+import discord
 from discord.ext import commands, tasks
 from utils.data_manager import load_user_data, save_user_data, load_all_users
 import logging
+
+# Configuración del logging
+logging.basicConfig(level=logging.DEBUG)
 
 
 class Economy(commands.Cog):
@@ -12,44 +16,70 @@ class Economy(commands.Cog):
     @commands.command(name='registrar')
     async def register_user(self, ctx):
         user_id = str(ctx.author.id)
-        if user_id in self.data:
+        guild_id = str(ctx.guild.id)
+        # Asegúrate de que la clave se forma correctamente
+        key = f"{user_id}_{guild_id}"
+        user_data = load_user_data(user_id, guild_id)
+        if user_data:
             await ctx.send(f'{ctx.author.name}, ya estás registrado.')
         else:
-            self.data[user_id] = {'balance': 1000}
-            save_user_data(user_id, 1000)
-            await ctx.send(f'{ctx.author.name}, has sido registrado con un saldo inicial de 1000 pesos chilenos.')
+            self.data[key] = {'guild_id': guild_id, 'balance': 1000}
+            save_user_data(user_id, guild_id, 1000)
+            await ctx.send(f'{ctx.author.name}, has sido registrado con un saldo inicial de 1000 MelladoCoins.')
 
     @commands.command(name='saldo')
     async def check_balance(self, ctx):
         user_id = str(ctx.author.id)
-        user_data = load_user_data(user_id)
+        guild_id = str(ctx.guild.id)
+        user_data = load_user_data(user_id, guild_id)
         if user_data:
             balance = user_data['balance']
-            await ctx.send(f'{ctx.author.name}, tu saldo es {balance} pesos chilenos.')
+            await ctx.send(f'{ctx.author.name}, tu saldo es {balance} MelladoCoins.')
         else:
             await ctx.send(f'{ctx.author.name}, no estás registrado. Usa el comando !registrar para registrarte.')
 
-    def update_balance(self, user_id, amount):
-        user_data = load_user_data(user_id)
+    def update_balance(self, user_id, guild_id, amount):
+        user_data = load_user_data(user_id, guild_id)
         if user_data:
             user_data['balance'] += amount
-            save_user_data(user_id, user_data['balance'])
+            save_user_data(user_id, guild_id, user_data['balance'])
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if not message.author.bot:
             user_id = str(message.author.id)
-            if user_id in self.data:
-                self.update_balance(user_id, 0.1)
+            guild_id = str(message.guild.id)
+            key = f"{user_id}_{guild_id}"
+            if key in self.data:
+                self.update_balance(user_id, guild_id, 0.1)
 
     @tasks.loop(minutes=5)
     async def passive_income(self):
-        for user_id in self.data:
-            self.update_balance(user_id, 0.01)
+        for key, user_data in self.data.items():
+            try:
+                user_id, guild_id = key.split("_")
+                self.update_balance(user_id, guild_id, 0.01)
+            except ValueError:
+                logging.error(
+                    f"Key '{key}' does not have the expected format 'user_id_guild_id'")
 
     @passive_income.before_loop
     async def before_passive_income(self):
         await self.bot.wait_until_ready()
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logging.debug("Bot is ready. Verifying members registration...")
+        for guild in self.bot.guilds:
+            guild_id = str(guild.id)
+            for member in guild.members:
+                if not member.bot:
+                    user_id = str(member.id)
+                    key = f"{user_id}_{guild_id}"
+                    if key not in self.data:
+                        self.data[key] = {
+                            'guild_id': guild_id, 'balance': 1000}
+                        save_user_data(user_id, guild_id, 1000)
 
 
 async def setup(bot):
