44baf7de486815640e979c351a2e47fbb27d7531
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2024-09-03 23:00:31 -0400

    feat: Add Summoner model for Riot API integration

---
+++ riot/models/summoner.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+
+class Summoner:
+    def __init__(self):
+        pass
+
+    def __repr__(self) -> str:
+        pass
+
+    def __str__(self) -> str:
+        pass
