4f4e5db80473e5d2c73c91728b96fee846b1681e
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2024-09-04 20:56:52 -0400

    feat: Update Summoner class to include all necessary attributes
    This commit updates the Summoner class in the riot/models/summoner.py file to include all the necessary attributes for representing summoner information. The __init__ method now accepts the id, accountId, puuid, profileIconId, revisionDate, and summonerLevel parameters and assigns them to the corresponding instance variables. The __repr__ and __str__ methods have also been implemented to provide a string representation of the Summoner object.

--- riot/models/summoner.py
+++ riot/models/summoner.py
@@ -2,11 +2,18 @@ from typing import Optional
 
 
 class Summoner:
-    def __init__(self):
-        pass
+    def __init__(self, id: str, accountId: str, puuid: str, profileIconId: int, revisionDate: int, summonerLevel: int):
+        self.id: str = id 
+        self.accountId: str = accountId
+        self.puuid: str = puuid
+        self.profileIconId: int = profileIconId
+        self.revisionDate: int = revisionDate
+        self.summonerLevel: int = summonerLevel
 
     def __repr__(self) -> str:
-        pass
+        return f"<Summoner NameID={self.id}, Level={self.summonerLevel}>"
 
     def __str__(self) -> str:
-        pass
+        return (f"Summoner(ID={self.id}, Level={self.summonerLevel}, "
+                f"ProfileIconId={self.profileIconId}, AccountId={self.accountId}, "
+                f"PUUID={self.puuid}, LastUpdated={self.revisionDate})")
