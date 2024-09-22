1dc01ba736b68a8b61bbd2e3dd91dfd217197e5f
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2024-09-22 19:01:51 -0300

    .

--- market_module/const_market.py
+++ market_module/const_market.py
@@ -1,8 +1,3 @@
-import random
-import numpy as np  # Librería para cálculos numéricos y estadísticos
-
-
-# Lista de colores disponibles para las propiedades
 COLORS = [
     '#FF0000',  # Rojo (Red)
     '#0000FF',  # Azul (Blue)
@@ -13,11 +8,11 @@ COLORS = [
     '#FFC0CB'   # Rosa (Pink)
 ]
 
-# Distribución de probabilidad para los barrios (no lineal)
+
 BARRIOS = list(range(1, 11))
 BARRIO_WEIGHTS = [0.3, 0.2, 0.15, 0.1, 0.1, 0.05, 0.03, 0.03, 0.02, 0.02]
 
-# Nombres según tipo y nivel de propiedad
+
 NOMBRES_HOGAR = {
     1: ['Puente', 'Calle', 'Esquina', 'Paradero', 'Terminal', 'Feria', 'Casa Abandonada', 'Un Árbol', 'Dos Árboles', 'Carpa', 'Baño'],
     2: ['Casa', 'Departamento', 'Piso Compartido', 'La Posada', 'Hostal', 'Cabaña'],
@@ -34,7 +29,7 @@ NOMBRES_TIENDA = {
     5: ['Rascacielos', 'Compañía Internacional', 'Centro Financiero']
 }
 
-# Tiers de calidad y multiplicadores
+
 TIERS = {
     'F': -0.30,
     'E': -0.15,
@@ -47,10 +42,9 @@ TIERS = {
     'S++': 0.90
 }
 
-# Pesos de probabilidad para los tiers
-TIER_WEIGHTS = [0.04, 0.13, 0.20, 0.30, 0.25, 0.05, 0.02, 0.007, 0.003]
 
-# Rango de valores de compra basado en el nivel
+TIER_WEIGHTS = [0.04, 0.13, 0.20, 0.30, 0.25, 0.05, 0.02, 0.00007, 0.00003]
+
 VALORES_BASE_NIVEL = {
     1: (10, 1000),
     2: (1001, 10000),
@@ -59,5 +53,5 @@ VALORES_BASE_NIVEL = {
     5: (1000000000, 50000000000)
 }
 
-# Pesos de probabilidad para los niveles
+
 NIVEL_WEIGHTS = [0.39, 0.40, 0.17, 0.03, 0.01]
