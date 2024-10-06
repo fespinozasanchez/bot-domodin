c0da1faa61b3cb2d138be5fe80dfbd1dc449852e
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2024-10-06 03:47:03 -0300

    Refactorizar configuración del formato de registro en logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -2,7 +2,7 @@ import logging
 
 
 def setup_logger():
-    # Configurar logging para discord.log
+    # Logger para discord.log (logs generales del bot)
     discord_logger = logging.getLogger('discord')
     if not discord_logger.handlers:  # Evitar múltiples handlers
         discord_handler = logging.FileHandler(filename='logs/discord.log', encoding='utf-8', mode='w')
@@ -11,7 +11,7 @@ def setup_logger():
         discord_logger.addHandler(discord_handler)
         discord_logger.setLevel(logging.INFO)
 
-    # Configurar logging para commands.log
+    # Logger para commands.log
     command_logger = logging.getLogger('command_logger')
     if not command_logger.handlers:  # Evitar múltiples handlers
         command_handler = logging.FileHandler(filename='logs/commands.log', encoding='utf-8', mode='a')
@@ -23,14 +23,14 @@ def setup_logger():
     # Remover handlers del root logger para evitar impresión en consola
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.WARNING)
-    for handler in root_logger.handlers[:]:
-        root_logger.removeHandler(handler)
+    if root_logger.hasHandlers():
+        root_logger.handlers.clear()
 
     # Asegurarse de que no haya propagación hacia el root logger
     discord_logger.propagate = False
     command_logger.propagate = False
 
-    # Para urllib3, si quieres que vaya a discord.log
+    # Configurar urllib3 para que vaya a discord.log
     urllib3_logger = logging.getLogger('urllib3')
     if not urllib3_logger.handlers:  # Evitar múltiples handlers
         urllib3_logger.addHandler(discord_handler)
