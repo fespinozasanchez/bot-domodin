b359555ce1c6ad9547f8621802d744474815516e
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2024-10-06 02:55:06 -0300

    Refactorizar configuración del formato de registro en logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -2,12 +2,30 @@ import logging
 
 
 def setup_logger():
+    # Logger para discord.log
     handler = logging.FileHandler(
         filename='logs/discord.log', encoding='utf-8', mode='w')
 
+    # Logger para commands.log
+    command_handler = logging.FileHandler(
+        filename='logs/commands.log', encoding='utf-8', mode='a')  # modo 'a' para añadir al archivo
+
     # Configuración del formato con fecha y hora
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    # Configuración para discord.log
+    handler.setFormatter(formatter)
+
+    # Configuración para commands.log
+    command_handler.setFormatter(formatter)
+
     logging.basicConfig(
         handlers=[handler],
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s'  # Incluyendo fecha y hora
     )
+
+    # Logger específico para comandos
+    command_logger = logging.getLogger('command_logger')
+    command_logger.setLevel(logging.INFO)
+    command_logger.addHandler(command_handler)
