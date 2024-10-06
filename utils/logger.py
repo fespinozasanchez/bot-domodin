84f677e28e782f18f67c07d5d7bed5f095b732cd
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2024-10-06 03:43:16 -0300

    Refactorizar configuración del formato de registro en logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -3,38 +3,25 @@ import logging
 
 def setup_logger():
     # Logger para discord.log (logs generales del bot)
-    discord_handler = logging.FileHandler(
-        filename='logs/discord.log', encoding='utf-8', mode='w')
-    discord_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    discord_handler.setFormatter(discord_formatter)
-
     discord_logger = logging.getLogger('discord')
-    discord_logger.setLevel(logging.INFO)
-    discord_logger.addHandler(discord_handler)
+    if not discord_logger.handlers:  # Evitar múltiples handlers
+        discord_handler = logging.FileHandler(filename='logs/discord.log', encoding='utf-8', mode='w')
+        discord_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        discord_handler.setFormatter(discord_formatter)
+        discord_logger.setLevel(logging.INFO)
+        discord_logger.addHandler(discord_handler)
 
-    # Logger para commands.log (logs de los comandos ejecutados)
-    command_handler = logging.FileHandler(
-        filename='logs/commands.log', encoding='utf-8', mode='a')
-    command_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    command_handler.setFormatter(command_formatter)
-
+    # Logger para commands.log
     command_logger = logging.getLogger('command_logger')
-    command_logger.setLevel(logging.INFO)
-    command_logger.addHandler(command_handler)
+    if not command_logger.handlers:  # Evitar múltiples handlers
+        command_handler = logging.FileHandler(filename='logs/commands.log', encoding='utf-8', mode='a')
+        command_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        command_handler.setFormatter(command_formatter)
+        command_logger.setLevel(logging.INFO)
+        command_logger.addHandler(command_handler)
 
-    # Eliminar la propagación para que los logs de comandos no vayan al logger global
-    command_logger.propagate = False
-
-    # Configurar los logs de urllib3 para que vayan a discord.log
-    urllib3_logger = logging.getLogger('urllib3')
-    urllib3_logger.setLevel(logging.INFO)
-    urllib3_logger.addHandler(discord_handler)  # Agregar handler para escribir en discord.log
-    urllib3_logger.propagate = False
-
-    # Remover handlers del root logger para que no imprima en la consola
+    # Eliminar handlers del root logger que imprimen en la consola
     root_logger = logging.getLogger()
+    root_logger.setLevel(logging.WARNING)
     for handler in root_logger.handlers[:]:
         root_logger.removeHandler(handler)
-
-    # Evitar que root logger imprima en consola, pero permitir que lo haga en archivos
-    root_logger.setLevel(logging.WARNING)
