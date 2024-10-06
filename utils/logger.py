782bd8eb2f726cc6e883ec3c084cbe96fadf8d19
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2024-10-06 03:31:04 -0300

    Refactorizar configuración del formato de registro en logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -2,30 +2,25 @@ import logging
 
 
 def setup_logger():
-    # Logger para discord.log
-    handler = logging.FileHandler(
+    # Logger para discord.log (logs generales del bot)
+    discord_handler = logging.FileHandler(
         filename='logs/discord.log', encoding='utf-8', mode='w')
+    discord_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    discord_handler.setFormatter(discord_formatter)
 
-    # Logger para commands.log
+    discord_logger = logging.getLogger('discord')
+    discord_logger.setLevel(logging.INFO)
+    discord_logger.addHandler(discord_handler)
+
+    # Logger para commands.log (logs de los comandos ejecutados)
     command_handler = logging.FileHandler(
         filename='logs/commands.log', encoding='utf-8', mode='a')  # modo 'a' para añadir al archivo
+    command_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    command_handler.setFormatter(command_formatter)
 
-    # Configuración del formato con fecha y hora
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-    # Configuración para discord.log
-    handler.setFormatter(formatter)
-
-    # Configuración para commands.log
-    command_handler.setFormatter(formatter)
-
-    logging.basicConfig(
-        handlers=[handler],
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'  # Incluyendo fecha y hora
-    )
-
-    # Logger específico para comandos
     command_logger = logging.getLogger('command_logger')
     command_logger.setLevel(logging.INFO)
     command_logger.addHandler(command_handler)
+
+    # Eliminar propagación para que los logs de comandos no vayan al logger global
+    command_logger.propagate = False
