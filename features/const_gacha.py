7bb74c5b58d3540632ff2af58c9a3f9231002be2
Mario <mariocastillosan@gmail.com>
2024-10-17 20:55:30 -0300

    Feature: Slot machine dict
    The slot machine gacha game dict

---
+++ features/const_gacha.py
@@ -0,0 +1,18 @@
+slot_combinations = {
+    "🍒": 2,  # pays x2
+    "🍋": 3,  # pays x3
+    "🍉": 5,  # pays x5
+    "🔔": 10, # pays x10
+    "⭐": 20  # pays x20
+
+}
+
+super_slot_combinations = {
+    "🍒": 50,  # pays x50
+    "🍋": 100,  # pays x100
+    "🍉": 150,  # pays x150
+    "🔔": 200, # pays x200
+    "⭐": 250  # pays x250
+
+}
+
