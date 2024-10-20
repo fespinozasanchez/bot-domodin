8cc8d0e7198431a3eb727799524389461aa33fc5
Mario <mariocastillosan@gmail.com>
2024-10-20 19:04:08 -0300

    Refactor: managing sessions

--- rpg_module/rpg_utils/rpg_data_manager.py
+++ rpg_module/rpg_utils/rpg_data_manager.py
@@ -9,6 +9,7 @@ from rpg_module.Players.mage import Mage
 from rpg_module.Players.thieve import Thieve
 from rpg_module.Players.player import Player  # Importar Player también
 from rpg_module.Enemy.enemy import Enemy
+from sqlalchemy.exc import SQLAlchemyError
 
 # Crear el motor de SQLAlchemy
 DATABASE_URL = f"mysql+pymysql://{DATABASE_CONFIG['user']}:{DATABASE_CONFIG['password']}@{DATABASE_CONFIG['host']}/{DATABASE_CONFIG['database']}"
@@ -25,28 +26,39 @@ def init_alchemy_db():
 # Función para registrar un jugador
 def register_player(name, player_class):
     """Registra un nuevo jugador en la base de datos."""
-    if player_class.lower() == 'warrior':
-        new_player = Warrior(name=name)
-    elif player_class.lower() == 'mage':
-        new_player = Mage(name=name)
-    elif player_class.lower() == 'thieve':
-        new_player = Thieve(name=name)
-    else:
-        raise ValueError("Clase de jugador no válida. Debe ser 'warrior', 'mage' o 'thieve'.")
+    try:
+        if player_class.lower() == 'warrior':
+            new_player = Warrior(name=name)
+        elif player_class.lower() == 'mage':
+            new_player = Mage(name=name)
+        elif player_class.lower() == 'thieve':
+            new_player = Thieve(name=name)
+        else:
+            raise ValueError("Clase de jugador no válida. Debe ser 'warrior', 'mage' o 'thieve'.")
+        
+        session.add(new_player)
+        session.commit()
+        return new_player  # Retorna el jugador registrado
     
-    session.add(new_player)
-    session.commit()
-    return new_player
+    except Exception as e:
+        session.rollback()
+        raise e
 
 # Función para obtener un jugador por nombre
 def get_player_by_name(name):
     """Obtiene un jugador de la base de datos usando su nombre."""
-    return session.query(Player).filter_by(name=name).first()
+    player = session.query(Player).filter_by(name=name).first()
+    return player
+
 
 # Función para obtener todos los jugadores
 def get_all_players():
     """Obtiene todos los jugadores de la base de datos."""
-    return session.query(Player).all()
+    session = Session()
+    try:
+        return session.query(Player).all()
+    finally:
+        session.close()
 
 
 
@@ -55,11 +67,39 @@ def level_up_player(player):
     required_points = 100
     if player.stats_points >= required_points:
         player.level += 1
+        player.health += 10
+        player.mana += 10
+        player.strength += 5
+        player.intelligence += 5
+        player.agility += 5
+        if player.class_player == 'warrior':
+            player.strength += 5
+        elif player.class_player == 'mage':
+            player.intelligence += 5
+        elif player.class_player == 'thieve':
+            player.agility += 5
         player.stats_points -= required_points
         session.commit()
         return f"{player.name} ha subido al nivel {player.level}! Puntos restantes: {player.stats_points}"
     else:
         return f"{player.name} no tiene suficientes puntos de stats para subir de nivel. Necesita {required_points} puntos."
+    
+def revive_player(player):
+    """Revive al jugador si tiene suficientes puntos de estadísticas."""
+    
+    required_points = 10
+    try:
+        if player.stats_points >= required_points:
+            player.health = 100
+            player.mana = 100
+            player.stats_points -= required_points
+            session.commit()
+            return f"Has sido revivido! "
+        else:
+            return f"No tienes suficientes puntos de stats para revivir. Necesita {required_points} puntos."
+    except SQLAlchemyError as e:
+        session.rollback()
+        raise e
 
 
 init_alchemy_db()
