470b59cab2833f3eeba4d3ed21b2e88effdc8fa0
Mario <mariocastillosan@gmail.com>
2024-11-20 17:48:15 -0300

    Update: API
    Monopoly game  get info

--- API/main.py
+++ API/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from rpg_module.rpg_utils.rpg_data_manager import get_player_by_name, get_all_players
-
+from utils.natural_events_manager import get_current_natural_event
+from utils.market_data_manager import get_current_event, obtener_propiedad,obtener_pagos,obtener_propiedades_por_usuario,obtener_propiedades_por_color
 
 
 
@@ -11,15 +12,40 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "What are you looking for 🙄"}
 
 
 @app.get("/players/{player_name}")
 def read_item(player_name:str):
     return {"player_name": get_player_by_name(player_name)}
 
-@app.get("/all_players/")
+@app.get("/players/all_players/")
 def all_players():
     return {"players": get_all_players()}
+
+
+@app.get("/natural_events/")
+def events():
+    return {"events": get_current_natural_event()}
+
+@app.get("/market_data/")
+def market_data():
+    return {"market_data": get_current_event()}
+
+@app.get("/market_data/{propiedad}")
+def propiedad(propiedad:str):
+    return {"propiedad": obtener_propiedad(propiedad)}
+
+@app.get("/market_data/pagos/")
+def pagos():
+    return {"pagos": obtener_pagos()}
+
+@app.get("/market_data/propiedades/{inv_id}")
+def propiedades(inv_id:str):
+    return {"propiedades": obtener_propiedades_por_usuario(inv_id)}
+
+@app.get("/market_data/propiedades/color/{color}")
+def propiedades_color(color:str):
+    return {"propiedades": obtener_propiedades_por_color(color)}
 
 
