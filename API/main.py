ee71ba12312e4c6eeff880265031c33d83ada177
Mario <mariocastillosan@gmail.com>
2024-11-20 19:13:01 -0300

    Update: 404 handling info

--- API/main.py
+++ API/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from rpg_module.rpg_utils.rpg_data_manager import get_player_by_name, get_all_players
 from utils.natural_events_manager import get_current_natural_event
 from utils.market_data_manager import get_current_event, obtener_propiedad,obtener_pagos,obtener_propiedades_por_usuario,obtener_propiedades_por_color
@@ -16,8 +16,11 @@ def root():
 
 
 @app.get("/players/{player_name}")
-def read_item(player_name:str):
-    return {"player_name": get_player_by_name(player_name)}
+def read_item(player_name:str, response: Response):
+    player=get_player_by_name(player_name)
+    if not player:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return {"player_name": player}
 
 @app.get("/players/all_players/")
 def all_players():
@@ -33,19 +36,28 @@ def market_data():
     return {"market_data": get_current_event()}
 
 @app.get("/market_data/{propiedad}")
-def propiedad(propiedad:str):
-    return {"propiedad": obtener_propiedad(propiedad)}
+def propiedad(propiedad:str, response: Response):
+    propiedad=obtener_propiedad(propiedad)
+    if not propiedad:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return {"propiedad":propiedad}
 
 @app.get("/market_data/pagos/")
 def pagos():
     return {"pagos": obtener_pagos()}
 
 @app.get("/market_data/propiedades/{inv_id}")
-def propiedades(inv_id:str):
-    return {"propiedades": obtener_propiedades_por_usuario(inv_id)}
+def propiedades(inv_id:str, response: Response):
+    propiedades=obtener_propiedades_por_usuario(inv_id)
+    if not propiedades:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return {"propiedades":propiedades}
 
 @app.get("/market_data/propiedades/color/{color}")
-def propiedades_color(color:str):
-    return {"propiedades": obtener_propiedades_por_color(color)}
+def propiedades_color(color:str, response: Response):
+    propiedades=obtener_propiedades_por_color(color)
+    if not propiedades:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return {"propiedades": propiedades}
 
 
